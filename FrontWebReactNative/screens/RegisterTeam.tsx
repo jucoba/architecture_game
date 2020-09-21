b46215d57fe1d7fd712b01764a70cdd9e4ff1c69
Jubel Correa <alberto.correa@gmail.com>
2020-09-20 20:48:21 -0500

    Stylinig

--- FrontWebReactNative/screens/RegisterTeam.tsx
+++ FrontWebReactNative/screens/RegisterTeam.tsx
@@ -1,18 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native';
 import globalStyles from '../styles/globalStyles'
+
+const onPress = () => {};
 
 export default function RegisterTeam () {
     return (
         <View style={globalStyles.container}>
-            <Text>Nombre de tu equipo </Text>
+            <Text style={globalStyles.text}>Nombre de tu equipo </Text>
             <TextInput style={globalStyles.textInupt}/>
-            <Text>Tu Nombre </Text>
+            <Text style={globalStyles.text}>Tu Nombre </Text>
             <TextInput style={globalStyles.textInupt}/>
-            <Button
-                title="Crea tu equipo"
-            />
+            <TouchableOpacity onPress={onPress} style={globalStyles.button}>
+                <View >
+                    <Text>Crea tu equipo</Text>
+                </View>
+            </TouchableOpacity>
         </View>
     );
 
--- FrontWebReactNative/styles/globalStyles.tsx
+++ FrontWebReactNative/styles/globalStyles.tsx
@@ -8,15 +8,23 @@ const globalStyles = StyleSheet.create({
         flex: 1,
         flexDirection: 'column',
         backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-        margin:100,
+        alignItems: 'flex-start',
+        justifyContent: 'top',
     },
     textInupt: {
         borderColor: 'gray',
         borderWidth: 1,
         margin:5,
-    }
+    },
+    text: {
+        margin:5,
+    },
+    button: {
+        margin:5,
+        alignItems: "center",
+        backgroundColor: "#DDDDDD",
+        padding: 10
+    },
 }
 );
 
