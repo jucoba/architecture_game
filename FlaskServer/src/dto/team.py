cac9bdfc55a9a3a5b4a079e61f275396195a89aa
Jubel Correa <alberto.correa@gmail.com>
2020-09-28 22:04:12 -0500

    more tests

--- FlaskServer/src/dto/team.py
+++ FlaskServer/src/dto/team.py
@@ -1,9 +1,20 @@
+import random
+
+
 class Team:
+
+    _current_clients: int
+    _capacity: int
+    _current_balance: int
 
     def __init__(self, team_name, first_member):
         self.team_name = team_name
-        self.current_balance = 100000
-        self.arch_level = 1
+        self._current_balance = 100000
+        self._architecure_level = 1
+        self._can_receive_new_clients = False
+        self._capacity = 0
+        self._id = random.randint(1000, 9999)
+        self._current_clients = 0
 
     @property
     def name(self):
@@ -11,9 +22,24 @@ class Team:
 
     @property
     def balance(self):
-        return self.current_balance
+        return self._current_balance
 
     @property
     def architecture_level(self):
-        return self.arch_level
+        return self._architecure_level
 
+    @property
+    def can_receive_new_clients(self):
+        return self._can_receive_new_clients
+
+    @property
+    def capacity(self):
+        return self._capacity
+
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def current_clients(self):
+        return self._current_clients
--- FlaskServer/test_module/test_teams.py
+++ FlaskServer/test_module/test_teams.py
@@ -2,13 +2,10 @@ import unittest
 from src.dto.team import Team
 
 
-
 class TestTeams(unittest.TestCase):
-
 
     def setUp(self) -> None:
         self.a_team = Team('A Team', 'Anibal')
-
 
     def test_create_team_name_must_be_set(self):
         self.assertEqual(self.a_team.name, 'A Team')
@@ -19,6 +16,15 @@ class TestTeams(unittest.TestCase):
     def test_create_team_architecture_level_must_be_set(self):
         self.assertEqual(self.a_team.architecture_level, 1)
 
+    def test_create_team_can_receive_new_clients_must_be_set(self):
+        self.assertEqual(self.a_team.can_receive_new_clients, False)
 
+    def test_create_team_must_have_0_capacity(self):
+        self.assertEqual(self.a_team.capacity, 0)
 
+    def test_create_team_must_have_a_4_digit_id(self):
+        self.assertGreaterEqual(self.a_team.id, 1000)
+        self.assertLessEqual(self.a_team.id, 9999)
 
+    def test_create_team_must_have_0_clients(self):
+        self.assertEqual(self.a_team.current_clients, 0)
