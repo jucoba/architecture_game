c28a5320cc332b4e2fe7ab50fd333f4189798905
Jubel Correa <alberto.correa@gmail.com>
2020-09-29 22:39:36 -0500

    create json parser

--- FlaskServer/app.py
+++ FlaskServer/app.py
@@ -1,17 +1,19 @@
 from flask import Flask, request
-from flask import render_template
+from src.parser.json_parser import JsonPareser
 
 import random
-
-
+import sys
 
 app = Flask(__name__, static_url_path='/')
+
 
 @app.route("/app")
 def home():
     return app.send_static_file('index.html')
 
+
 @app.route('/register_team', methods=['GET', 'POST'])
 def register_team():
     print(request.json)
-    return str(random.randint(1000,9999))
+    print(sys.path)
+    return str(random.randint(1000, 9999))
