85c879f110adac5e53ea140c91dd6e32c0005d50
Jubel Correa <alberto.correa@gmail.com>
2020-10-03 08:45:57 -0500

    Playing with useEffect hook

--- FrontWebReactNative/screens/TeamPanel.tsx
+++ FrontWebReactNative/screens/TeamPanel.tsx
@@ -1,15 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import React, {useState} from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 import globalStyles from '../styles/globalStyles';
 import { get_team_api } from '../ApiCalls/get_team_api';
 
 
+
 export default function TeamPanel( {route, navigation }) {
     const { value } = route.params;
-    let id = JSON.stringify(value);
-    console.log('value ' + JSON.stringify(value));
+    const [id, setId] = useState(JSON.stringify(value));
+
+    const [json_team, setJson_team] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(
+        () => {
+            fetch('http://localhost:5000/team/'+id)
+                .then((response) => response.json())
+                .then((json) => setJson_team(json) )
+                .catch((error) => console.log(error)  )
+                .finally( () => setLoading(false) )
+        }, [id]
+    );
+
     return (  <View style={globalStyles.container}>
                 <Text>hola {id}</Text>
+                <TextInput
+                    onChangeText = {(text) => setId(text) }
+                />
               </View> );
 };
